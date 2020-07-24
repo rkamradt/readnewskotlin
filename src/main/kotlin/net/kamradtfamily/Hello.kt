a1ba11b36c37fe60bbc56d8e6b49871631f7b796
Randal Kamradt <randykamradt@gmail.com>
2020-07-24 08:07:59 -0700

    initial commit

---
+++ src/main/kotlin/net/kamradtfamily/Hello.kt
@@ -0,0 +1,8 @@
+package net.kamradtfamily
+
+@SpringBootApplication
+class KotlinDemoApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KotlinDemoApplication::class.java, *args)
+}
---
+++ src/test/kotlin/net/kamradtfamily/HelloTest.kt
@@ -0,0 +1,8 @@
+package net.kamradtfamily
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
